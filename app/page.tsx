3ac25d2a9bc0828baa65817b22cfa339c92f11bd
FITTOBR <manfredomelo20@gmail.com>
2026-02-27 09:39:39 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{padding: 40}}>
+    <main style={{ padding: 40 }}>
       <h1>Fitto está no ar 🚀</h1>
-      <p>Sistema pronto para lojistas.</p>
+      <p>Ambiente de testes para lojistas.</p>
     </main>
   )
 }
